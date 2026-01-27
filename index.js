ae6b2c7f19953c30bd24968a31b1bb4684f8e49d
rajesh19221 <awsdeployweb@gmail.com>
2026-01-27 18:11:22 +0530

    Resolve index.js conflict and finalize automation

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
-// Default route
-app.get('/', (req, res) => {
-  res.send('Hello, Rajesh  Rawat! This is your Node.js app!');
-});
-
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`);
-});
