8d4d52375421fdd6641f955c05d84c523c220d04
rajesh19221 <awsdeployweb@gmail.com>
2026-01-27 18:06:15 +0530

    Finalize CodeDeploy automation

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
-set -e
-
-echo "Starting application with PM2"
+i#!/bin/bash
 
 APP_DIR="/home/ubuntu/testcode"
 APP_NAME="nodejs-app"
+APP_FILE="index.js"
 
-cd "$APP_DIR"
+cd "$APP_DIR" || exit 1
 
 npm install --omit=dev
 
 pm2 stop "$APP_NAME" || true
 pm2 delete "$APP_NAME" || true
-pm2 start app.js --name "$APP_NAME"
+pm2 start "$APP_DIR/$APP_FILE" --name "$APP_NAME"
 pm2 save
+
+exit 0
+
+
